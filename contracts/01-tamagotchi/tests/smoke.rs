00c2e046f58e546b848ce0c80608d96d8537f200
jonar12 <jona.arredondo12@gmail.com>
2024-01-19 16:45:03 -0600

    Fixed Age action test

--- contracts/01-tamagotchi/tests/smoke.rs
+++ contracts/01-tamagotchi/tests/smoke.rs
@@ -1,6 +1,8 @@
 use gtest::{Log, Program, System};
 use tamagotchi_io::{TmgAction, TmgEvent};
 
+const TEST_AGE: u64 = 30;
+
 #[test]
 fn smoke_test() {
     let sys = System::new();
@@ -17,10 +19,10 @@ fn smoke_test() {
         .payload(TmgEvent::Name(String::from("Tamagotchi Name")));
     assert!(res.contains(&expected_log));
 
+    sys.spend_blocks(TEST_AGE as u32);
     let res = _program.send(2, TmgAction::Age);
-    sys.spend_blocks(150);
     let expected_log = Log::builder()
         .dest(2)
-        .payload(TmgEvent::Age(150));
+        .payload(TmgEvent::Age(TEST_AGE * 1000));
     assert!(res.contains(&expected_log));
 }
